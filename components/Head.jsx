bfba04c27edf334ee7153e6d009b8b452039cb72
jeromeguban <jeromeguban02@gmail.com>
2023-07-03 09:15:35 +0800

    Update Head.jsx

--- components/Head.jsx
+++ components/Head.jsx
@@ -17,7 +17,7 @@ const CustomHead = ({ title }) => {
         property="og:description"
         content="A full-stack developer building websites that you'd like to use."
       />
-      <meta property="og:image" content="https://previews.dropbox.com/p/thumb/AB_LccY2BT0E-yqRG-vPAIwI7bpsDN-CAndg9i8QKHTj4AN18R6pPGpFK7Co92k9ZdYhhGaoGHHM6X2SQJ2vtc89HUVfxJjPRsb9QX9bGgmzs00_p1gQYsU-H92FLwcI6xwfWG1ELjeaVghxK93U3SYa_xkx4mEqaqhdDG6OBzyURkDvrKi6wLoPRXIfoNwQsIgJ9GUluKHN2ssWkT21JDjPyEnUfQ3Q7kEreHZ7ePrrN89G8lXUg6_yz4zamOuiu-0BLMgsnbS8zQWllb3At1aCqH9LCfqpf4GVKX1LTnZpgkhTp5bRTXFnfXf4RuWCUWwsAvV3GXrhWLltVnU12EpA/p.jpeg" />
+      <meta property="og:image" content="https://i.imgur.com/4zi5KkQ.png" />
       <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
